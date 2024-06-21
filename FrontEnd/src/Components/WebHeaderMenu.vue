c07997bc3db66fe5885fe79271e85f681e8a3c02
admin_signin <siem.choub@student.passerellesnumeriques.org>
2024-06-21 08:59:35 +0700

    test

--- FrontEnd/src/Components/WebHeaderMenu.vue
+++ FrontEnd/src/Components/WebHeaderMenu.vue
@@ -2,11 +2,11 @@
 import { Icon } from '@iconify/vue'
 </script>
 <template>
-  <header class="flex justify-between px-50 py-3 bg-white items-center">
+  <header class="flex justify-between px-20 py-3 bg-white items-center">
     <!-- Logo -->
     <div class="flex items-center space-x-2">
-      <Icon icon="skill-icons:devto-dark" style="font-size: 45px" />
-      <span class="text-xl font-bold">I-KNOW</span>
+      <Icon icon="skill-icons:Circleto-dark" style="font-size: 45px" />
+      <span class="text-xl font-bold " style="font-size:35px">QUICKFIX</span>
     </div>
 
     <!-- Menu Items -->
@@ -14,28 +14,34 @@ import { Icon } from '@iconify/vue'
       <a
         href="/post"
         class="font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900"
-        >Post</a
+        >Home</a
       >
       <a
         href="/team"
         class="font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900"
-        >Team</a
+        >Service</a
       >
       <a
         href="/projects"
         class="font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900"
-        >Projects</a
+        >About</a
       >
       <a
         href="/reports"
         class="font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900"
-        >Reports</a
+        >Contact</a
       >
     </nav>
 
     <!-- Sign In -->
-    <div>
+    <div class="d-flex">
+      <div class="px-4 py-2 rounded font-semibold btn btn-danger">Sign Up</div>
       <div class="px-4 py-2 rounded font-semibold">Sign In</div>
     </div>
   </header>
 </template>
+<style scoped>
+  a{
+    text-decoration: none;
+  }
+</style>
