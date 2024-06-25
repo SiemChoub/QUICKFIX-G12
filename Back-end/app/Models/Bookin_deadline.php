433cd85114476dfa771a1b094a6708040e6444fb
admin_signin <siem.choub@student.passerellesnumeriques.org>
2024-06-25 08:23:22 +0700

    #QC-28 both back and front

---
+++ Back-end/app/Models/Bookin_deadline.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Bookin_deadline extends Model
+{
+    use HasFactory;
+}
