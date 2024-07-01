779f40e5ec6baffab3560e6cf7d29c1e27db87d2
koeuk-Developer <koeukkos@gmail.com>
2024-07-01 08:03:30 +0700

    merge code

--- Back-end/app/Models/Service.php
+++ Back-end/app/Models/Service.php
@@ -7,10 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Service extends Model
 {
     use HasFactory;
+<<<<<<< HEAD
     protected $fillable = ['name','description','image','price','category_id'];
 
     public function category()
     {
         return $this->belongsTo(Category::class);
     }
+=======
+    protected $fillable = ['name','description','image','price'];
+>>>>>>> 2adbd5d (merge service)
 }
