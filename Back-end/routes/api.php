c5eb6707c6a81182dc95c0e241012da94332ccc1
penh sovannarith <sovannarith.penh@student.passerellesnumeriques.org>
2024-07-01 20:11:21 +0700

    category-data

--- Back-end/routes/api.php
+++ Back-end/routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\API\PostController;
+use App\Http\Controllers\API\ServiceController;
+use App\Http\Controllers\API\CategoryController;
 use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -24,3 +26,6 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::post('/logout', [AuthController::class, 'logout'])-> middleware('auth:sanctum');
 Route::get('/me', [AuthController::class, 'index'])->middleware('auth:sanctum');
 Route::get('/post/list', [PostController::class, 'index'])->middleware('auth:sanctum');
+
+Route::get('/service/list', [ServiceController::class, 'index']);
+Route::get('/category/list', [CategoryController::class, 'index']);
