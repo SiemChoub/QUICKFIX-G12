ed72c8c9de784e25b7dfe011bcda5015c8dc8d4e
THIDA.HIM <THIDA.HIM@pnc.lan>
2024-07-01 18:54:19 +0700

    update profile

--- Back-end/app/Http/Controllers/Admin/UserController.php
+++ Back-end/app/Http/Controllers/Admin/UserController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
@@ -53,32 +55,56 @@ class UserController extends Controller
         return view('setting.user.edit', ['user' => $user]);
     }
 
-    // app/Http/Controllers/UserController.php
-    public function update(Request $request)
-    {
-        // Validate the request data
-        $request->validate([
-            'role' => 'required',
-            'name' => 'required',
-            'email' => 'required|email',
-            'phone' => 'required',
-        ]);
+    public function updateProfile(Request $request)
+{
+    $validator = Validator::make($request->all(), [
+        'profile' => 'nullable|image|max:2048',
+    ]);
 
-        // Update the user data
-        $user = auth()->user();
-        $user->role = $request->input('role');
-        $user->name = $request->input('name');
-        $user->email = $request->input('email');
-        $user->phone = $request->input('phone');
-        $user->save();
-
-        return response()->json(['message' => 'User updated successfully']);
+    if ($validator->fails()) {
+        return response()->json(['errors' => $validator->errors()], 422);
     }
+
+    $user_id = Auth::id();
+    $user = User::find($user_id);
+
+    if ($request->hasFile('profile')) {
+        $image = $request->file('profile');
+        $imageBase64 = base64_encode(file_get_contents($image->getRealPath()));
+
+        $base64String = 'data:image/' . $image->getClientOriginalExtension() . ';base64,' . $imageBase64;
+        $user->profile = $base64String;
+    }
+
+    $user->save();
+
+    return response()->json([
+        'message' => 'User profile image updated successfully',
+        'user' => $user,
+    ], 200);
+}
 
     public function destroy(User $user)
     {
         $user->delete();
 
         return redirect()->back()->withSuccess('User deleted!');
+    }
+
+    public function updateInformation(Request $request)
+    {
+        // Validate the request data
+        $request->validate([
+            'name' => 'required',
+            'phone' => 'required',
+        ]);
+
+        // Update the user data
+        $user = Auth::user();
+        $user->name = $request->input('name');
+        $user->phone = $request->input('phone');
+        $user->save();
+
+        return response()->json(['message' => 'User updated successfully']);
     }
 }
--- Back-end/database/migrations/2014_10_12_000000_create_users_table.php
+++ Back-end/database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,7 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('phone')->nullable();
+            $table->longText('profile')->nullable();
             $table->string('role')->default('user');
             $table->integer('location_id')->nullable();
             $table->rememberToken();
