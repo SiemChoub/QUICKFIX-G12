87c7e0d1ef09365637d11caa4e7d3baa3beb2068
admin_signin <siem.choub@student.passerellesnumeriques.org>
2024-07-16 12:31:27 +0700

    map

--- Back-end/app/Http/Controllers/API/Bookin_deadlineController.php
+++ Back-end/app/Http/Controllers/API/Bookin_deadlineController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Bookin_deadline;
 use App\Models\Booking;
-
+use App\Models\FixingProgress;
 
 class Bookin_deadlineController extends Controller
 {
@@ -21,49 +21,64 @@ class Bookin_deadlineController extends Controller
     }
     public function store(Request $request)
     {
-    /**
-     * Store a newly created resource in storage.
-     */
-    // $validatedData = $request->validate([
-    //     'service_id' => 'required|integer',
-    //     'date' => 'required|string',
-    //     'user_id' => 'required|integer',
-    //     'promotion_id' => 'nullable|integer',
-    //     'fixer_id' => 'nullable|integer',
-    //     'latitude' => 'required|string', 
-    //     'longitude' => 'required|string', 
-    // ]);
+        // Validate the request
+        // $validatedData = $request->validate([
+        //     'service_id' => 'required|integer',
+        //     'date' => 'required|string',
+        //     'user_id' => 'required|integer',
+        //     'promotion_id' => 'nullable|integer',
+        //     'fixer_id' => 'nullable|integer',
+        //     'latitude' => 'required|string', 
+        //     'longitude' => 'required|string', 
+        // ]);
+    
+        // Parse the location input
+        // $location = explode(',', $validatedData['location']);
+        // $latitude = $location[0];
+        // $longitude = $location[1];
+    
+        // Create a new Bookin_immediately record
+        $booking_deadline = new Bookin_deadline();
+        if(isset($request->service_id)) {
+            $booking_deadline->service_id = $request->service_id;
+        }
+        $booking_deadline->user_id = $request->user_id;
+        $booking_deadline->latitude = $request->latitude;
+        $booking_deadline->longitude = $request->longitude;
+        $booking_deadline->date = $request->date;
+        $booking_deadline->message = $request->message;
+        if(isset($requestpromotion_id)) {
+            $booking_deadline->promotion_id = $requestpromotion_id;
+        }
+        $booking_deadline->save();
+    
+        // Create a new Booking record
+        $booking = new Booking();
+        $booking->booking_type_id = $booking_deadline->id;
+        $booking->user_id = $request->user_id;
+        $booking->type = 'deadline';
+        if (isset($request->fixer_id)) {
+            $booking->fixer_id = $request->fixer_id;
+        }
+        $booking->save();
+        if (isset($request->fixer_id)) {
+            FixingProgress::create([
+               'fixer_id' => $request->fixer_id,
+               'booking_id' => $booking->id,
+               'action' => 'progress',
+           ]);
+           $booking = Booking::findOrFail($booking->id);
+           $booking->action = 'progress';
+           $booking->fixer_id = $request->fixer_id;
+           $booking->save();
 
-    // Parse the location input
-    // $location = explode(',', $validatedData['location']);
-    // $latitude = $location[0];
-    // $longitude = $location[1];
-
-    // Create a new Bookin_immediately record
-    $booking_deadline = new Bookin_deadline();
-    $booking_deadline->service_id = $request->service_id;
-    $booking_deadline->user_id = $request->user_id;
-    $booking_deadline->latitude = $request->latitude;
-    $booking_deadline->longitude = $request->longitude;
-    $booking_deadline->date = $request->date;
-    $booking_deadline->message = $request->message;
-    if(isset($request->promotion_id)) {
-        $booking_deadline->promotion_id = $request->promotion_id;
+        }
+    
+    
+        return response()->json($booking_deadline);
     }
-    $booking_deadline->save();
 
-    // Create a new Booking record
-    $booking = new Booking();
-    $booking->booking_type_id = $booking_deadline->id;
-    $booking->user_id = $request->user_id;
-    $booking->type = 'deadline';
-    if (isset($request->fixer_id)) {
-        $booking->fixer_id = $request->fixer_id;
-    }
-    $booking->save();
 
-    return response()->json($booking_deadline);
-}
 
     /**
      * Display the specified resource.
--- Back-end/routes/api.php
+++ Back-end/routes/api.php
@@ -34,9 +34,9 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
-Route::post('/logout', [AuthController::class, 'logout'])-> middleware('auth:sanctum');
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 Route::get('/me', [AuthController::class, 'index'])->middleware('auth:sanctum');
-Route::put('/update/{id}', [AuthController::class, 'updateInformation'])-> middleware('auth:sanctum');
+Route::put('/update/{id}', [AuthController::class, 'updateInformation'])->middleware('auth:sanctum');
 Route::post('/update/profile/{id}', [AuthController::class, 'updateProfile'])->middleware('auth:sanctum');
 
 Route::get('/fixer/list', [FixerController::class, 'index']);
@@ -45,7 +45,7 @@ Route::put('/profile/update/{id}', [AuthController::class, 'update'])->middlewar
 
 Route::get('/service/list', [ServiceController::class, 'index']);
 Route::get('/category/list', [CategoryController::class, 'index']);
-Route::resource('/booking',BookingController::class)->middleware('auth:sanctum');
+Route::resource('/booking', BookingController::class)->middleware('auth:sanctum');
 
 
 // ---------------- booking immedetely -------------
@@ -55,22 +55,22 @@ Route::put('/bookin_immediatly/{id}', [Bookin_memediatelyController::class, 'upd
 Route::delete('/bookin_immediatly/{id}', [Bookin_memediatelyController::class, 'destroy'])->middleware('auth:sanctum');
 
 // ---------------- booking deadline -------------
-    Route::get('/bookin_deadline', [Bookin_deadlineController::class, 'index'])->name('bookin_deadline.index');
-    Route::post('/bookin_deadline/create', [Bookin_deadlineController::class, 'store'])->name('bookin_deadline.create');
-    Route::post('/bookin_deadline', [Bookin_deadlineController::class, 'store'])->name('bookin_deadline.store');
-    Route::get('/bookin_deadline/{bookin_deadline}', [Bookin_deadlineController::class, 'show'])->name('bookin_deadline.show');
-    Route::get('/bookin_deadline/{bookin_deadline}/edit', [Bookin_deadlineController::class, 'edit'])->name('bookin_deadline.edit');
-    Route::put('/bookin_deadline/{bookin_deadline}', [Bookin_deadlineController::class, 'update'])->name('bookin_deadline.update');
-    Route::delete('/bookin_deadline/{bookin_deadline}', [Bookin_deadlineController::class, 'destroy'])->name('bookin_deadline.destroy');
+Route::get('/bookin_deadline', [Bookin_deadlineController::class, 'index'])->name('bookin_deadline.index');
+// Route::post('/bookin_deadline/create', [Bookin_deadlineController::class, 'store'])->name('bookin_deadline.create');
+Route::post('/bookin_deadline', [Bookin_deadlineController::class, 'store'])->name('bookin_deadline.store');
+Route::get('/bookin_deadline/{bookin_deadline}', [Bookin_deadlineController::class, 'show'])->name('bookin_deadline.show');
+Route::get('/bookin_deadline/{bookin_deadline}/edit', [Bookin_deadlineController::class, 'edit'])->name('bookin_deadline.edit');
+Route::put('/bookin_deadline/{bookin_deadline}', [Bookin_deadlineController::class, 'update'])->name('bookin_deadline.update');
+Route::delete('/bookin_deadline/{bookin_deadline}', [Bookin_deadlineController::class, 'destroy'])->name('bookin_deadline.destroy');
 
 
 
-Route::resource('/fixing_progressing',FixingProgressController::class)->middleware('auth:sanctum');
+Route::resource('/fixing_progressing', FixingProgressController::class)->middleware('auth:sanctum');
 
-Route::get('/service',[AuthController::class, 'index'])->name('service');
-Route::get('/discount',[PromotionController::class, 'index'])->name('service');
+Route::get('/service', [AuthController::class, 'index'])->name('service');
+Route::get('/discount', [PromotionController::class, 'index'])->name('service');
 
-Route::get('/promotion',[PromotionController::class, 'index'])->name('promotion');
+Route::get('/promotion', [PromotionController::class, 'index'])->name('promotion');
 // Route::get('/promotion',[PromotionController::class, 'index'])->name('promotion');
 Route::post('/promotion/create', [PromotionController::class, 'store'])->name('promotion.create');
 Route::get('/promotion/show/{id}', [PromotionController::class, 'show'])->name('promotion.show');
