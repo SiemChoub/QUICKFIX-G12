b71153836152cd0be7fb814343095c7ae27e42ff
admin_signin <siem.choub@student.passerellesnumeriques.org>
2024-07-17 09:58:37 +0700

    push

--- Back-end/app/Http/Controllers/API/ServiceController.php
+++ Back-end/app/Http/Controllers/API/ServiceController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\ServiceResource;
 use App\Models\Service;
 use Illuminate\Http\Request;
 
@@ -11,12 +12,11 @@ class ServiceController extends Controller
     public function index()
 {
     $services = Service::all();
-    $serviceCount = $services->count();
+    $services = ServiceResource::Collection($services);
     $topService = $services->sortByDesc('rating')->take(3);
 
     return response()->json([
         'services' => $services,
-        'service_count' => $serviceCount,
         'topService' => $topService,
     ]);
 }
